0b417f34226cfa44ac5b009c84ff0e79e44b35b7
sparlit <simonpeterarl@gmail.com>
2026-09-04 07:07:46 +0300

    Saving files before refreshing line endings

---
+++ atssetup.bat
@@ -0,0 +1,8 @@
+@echo off
+git pull origin main --force
+mypy .
+mypy . --strict
+mypy . --strict -n 10 --check-untyped-defs
+mypy . --strict -n 10 --check-untyped-defs --exclude-gitignore --install-types --non-interactive --warn-incomplete-stub --pdb --show-traceback --no-fixed-format-cache --show-absolute-path --show-error-end --show-error-context --extra-checks
+pyflakes .
+python -m pytest -n auto -vv -s --count=5 --full-trace --cache-clear -k "not gui_integration"
